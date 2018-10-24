a1f8ebca52149f4a8bd3b61af63be888f61a27a4
andreas-optimizely <andreas@optimizely.com>
2018-10-24 09:37:33 -0700

    add check for builds dir to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,18 @@ const dir = './builds';
 const zip = require('gulp-vinyl-zip');
 
 gulp.task('default', function() {
-  // place code for your default task here
   let numBuilds;
-
+  
+  if(!fs.existsSync(dir)){
+    console.log('Builds dir doesn\'t exist. Creating builds dir now');
+    fs.mkdirSync(dir)    
+  }
+  
   fs.readdir(dir, (err, files) => {
-    console.log(files.length);
     numBuilds = files.length;
+    
+    console.log('Build Number: ', numBuilds);
+
     let destination = 'builds/alexa-v' + numBuilds + '.zip';
     console.log(destination);
     return gulp.src(['**/*', 'gulpfile.js', '!builds', '!builds/**'])
